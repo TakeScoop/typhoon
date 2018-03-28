8578c99272dfa05fb8539cad56fc8eb87394f538
Ben Drucker <bvdrucker@gmail.com>
2018-03-28 14:48:57 -0700

    add outputs required for a dependent module to ssh to controllers.0

--- aws/container-linux/kubernetes/outputs.tf
+++ aws/container-linux/kubernetes/outputs.tf
@@ -2,3 +2,13 @@ output "ingress_dns_name" {
   value       = "${aws_lb.ingress.dns_name}"
   description = "DNS name of the network load balancer for distributing traffic to Ingress controllers"
 }
+
+output "bastion_dns_name" {
+  value       = "${aws_lb.bastion.dns_name}"
+  description = "DNS name of the network load balancer for distributing traffic to bastion hosts"
+}
+
+output "bootstrap_controller_ip" {
+  value       = "${aws_instance.controllers.0.private_ip}"
+  description = "IP address of the controller instance used to bootstrap the cluster"
+}
