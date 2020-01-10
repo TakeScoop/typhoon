2aed2e815c13d2e7f8c5ca525faaa5f6f52b1ef5
Erik Bryant <erik@takescoop.com>
2020-01-10 14:53:24 -0800

    Fix representation of source IPs

--- aws/container-linux/kubernetes/security.tf
+++ aws/container-linux/kubernetes/security.tf
@@ -199,11 +199,11 @@ resource "aws_security_group" "worker" {
 resource "aws_security_group_rule" "worker-ssh-bastion" {
   security_group_id = "${aws_security_group.worker.id}"
 
-  type      = "ingress"
-  protocol  = "tcp"
-  from_port = 22
-  to_port   = 22
-  cidr_blocks = [aws_security_group.bastion_internal.egress]
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 22
+  to_port                  = 22
+  source_security_group_id = aws_security_group.bastion_internal.id
 }
 
 resource "aws_security_group_rule" "worker-ssh-self" {
